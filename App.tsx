21d2aef9ea8b17dc61f2d2591c7cfcb411d21c66
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-11-11 13:04:54 +0530

    Fix import statement for TechnicianDashboard

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { Sidebar } from './components/Sidebar';
 import { Header } from './components/Header';                       
 import { AdminDashboard } from './pages/AdminDashboard                                        
 import { CustomerDashboard } from './pages/CustomerDashboard';         
-import { TechnicianDashboard } from './pages/TechnicianDashboard';                                 
+import { TechnicianDashboard } from './pages/TechnicianDashboard';                                   
   
 const SettingsPage: React.FC = () => (
     <Card title="Settings">                    
