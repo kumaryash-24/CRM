98bf804003027a64d6b6f06b477f08cbc34f989e
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-11-13 10:31:28 +0530

    Fix error handling for missing root element

--- index.tsx
+++ index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root'); 
-if (!rootElement) {   
+if (!rootElement) {      
   throw new Error("Could not find root element to mount to");  
 }  
 
